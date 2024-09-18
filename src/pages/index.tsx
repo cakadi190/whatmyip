8a8bde7c77cd8f4b23dd7392ac4f60f5e432b279
Amir Zuhdi Wibowo <94032140+cakadi190@users.noreply.github.com>
2024-09-18 19:42:42 +0700

    "Added react-tooltip dependency and updated index.tsx page with new imports and components"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
+import { IPinfo } from 'node-ipinfo';
 import { useEffect, useState } from 'react';
 
 interface IPAddresses {
-  ipv4: string | null;
-  ipv6: string | null;
+  ipv4?: string | null;
+  ipv6?: string | null;
+  ipinfo?: IPinfo;
 }
 
 const Home = () => {
@@ -34,7 +37,7 @@ const Home = () => {
         <meta property="og:title" content="Cek Alamat IP Anda" />
         <meta property="og:description" content="Cek alamat IPv4 dan IPv6 Anda secara instan." />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta property="og:url" content="https://whatmyip.cakadi190.eu.org/" />
         <meta property="og:image" content="https://yourwebsite.com/thumbnail.jpg" />
       </Head>
 
@@ -60,13 +63,18 @@ const Home = () => {
             <Link 
               target='_blank' 
               href={`http://[${ips.ipv6}]`} 
-              className="p-4 flex w-full justify-center rounded-full border leading-none mb-6 border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-200 font-mono text-2xl"
+              className="p-4 flex w-full justify-center rounded-full border leading-none mb-2 border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-200 font-mono text-2xl"
             >
               <div className="opacity-25 mr-1">http://</div>
               {ips.ipv6}
               <div className="opacity-25 ml-1">/</div>
             </Link>
           )}
+
+          <div className="mb-4 flex gap-2 justify-center align-items-center text-center">
+            <span>{ips.ipinfo && !ips.ipinfo.bogon ? `${ips.ipinfo.city}, ${ips.ipinfo.country}` : 'IP Lokal / Bogon'}</span>
+            {ips.ipinfo && !ips.ipinfo.bogon && <Image src={ips.ipinfo.countryFlagURL} alt={ips.ipinfo.city} width={32} height={24} />}
+          </div>
 
           <p className="text-gray-600 dark:text-gray-400">
             Perlu diperhatikan, bahwa semua informasi yang ditampilkan ini tidak selamanya akurat. Adakalanya alamat IP yang tampil itu sesuai dengan ISP yang bersangkutan. Silahkan hubungi penyedia layanan internet anda untuk mendapatkan informasi lebih lanjut.
